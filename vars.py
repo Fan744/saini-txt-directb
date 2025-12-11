3cde0f5288220752e3161a851b0fed3eb63d3cd6
Fan744 <135441979+Fan744@users.noreply.github.com>
2025-12-11 23:05:54 +0530

    Set default values for API credentials in vars.py
    Updated environment variable defaults for API_ID, API_HASH, and BOT_TOKEN.

--- vars.py
+++ vars.py
@@ -2,9 +2,9 @@
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+API_ID    = os.environ.get("API_ID", "30157302")
+API_HASH  = os.environ.get("API_HASH", "0052039fb2fca727868d0228cdaad569")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8548114461:AAHPh6ko0lx2iP4uXEiKoZ_mRZv9-UU7_vw") 
 
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
